11b9247fc4ee12852115483bb815cc1246b62205
gtz.jesus <gtz.jesus@outlook.com>
2023-08-18 18:00:39 -0600

    Buliding the Pages

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Product from './pages/Product.jsx';
 import Pricing from './pages/Pricing.jsx';
 import AppLayout from './pages/AppLayout.jsx';
 import PageNotFound from './pages/PageNotFound.jsx';
+import Login from './pages/Login.jsx';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
