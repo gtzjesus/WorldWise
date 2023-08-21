75fdee707e1157f01031b2c56d1363f95dc60802
gtz.jesus <gtz.jesus@outlook.com>
2023-08-21 13:25:05 -0600

    useNavigate Navigate Hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 
 import Homepage from './pages/Homepage.jsx';
@@ -9,6 +9,7 @@ import PageNotFound from './pages/PageNotFound.jsx';
 import Login from './pages/Login.jsx';
 import CityList from './components/CityList.jsx';
 import City from './components/City.jsx';
+import Form from './components/Form.jsx';
 import CountryList from './components/CountryList.jsx';
 
 const BASE_URL = 'http://localhost:8000';
@@ -40,10 +41,7 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route
-            index
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          ></Route>
+          <Route index element={<Navigate replace to="cities" />}></Route>
           <Route path="cities/:id" element={<City />}></Route>
           <Route
             path="cities"
@@ -54,7 +52,7 @@ function App() {
             path="countries"
             element={<CountryList cities={cities} isLoading={isLoading} />}
           ></Route>
-          <Route path="form" element={<p>Form</p>}></Route>
+          <Route path="form" element={<Form />}></Route>
         </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
