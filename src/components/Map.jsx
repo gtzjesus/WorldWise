ce23e9fe49d5acf09ce68b4aa3a7b7888cfe2c50
gtz.jesus <gtz.jesus@outlook.com>
2023-08-22 14:32:44 -0600

    Dynamic Form on Click Custom Hook + Query Manipulation

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,19 +1,42 @@
 import styles from './Map.module.css';
 
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMap,
+  useMapEvents,
+} from 'react-leaflet';
+
 import { useState } from 'react';
 import { useCities } from '../contexts/CityContext';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useEffect } from 'react';
 
 function Map() {
   const { cities } = useCities();
-  const [mapPosition] = useState([40, 0]);
+  const [mapPosition, setMapPosition] = useState([40, 0]);
+
+  const [searchParams] = useSearchParams();
+
+  const mapLat = searchParams.get('lat');
+  const mapLng = searchParams.get('lng');
+
+  useEffect(
+    function () {
+      if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
+    },
+    [mapLat, mapLng]
+  );
 
   return (
     <div className={styles.mapContainer}>
       <MapContainer
         className={styles.map}
         center={mapPosition}
-        zoom={13}
+        // center={[mapLat, mapLng]}
+        zoom={6}
         scrollWheelZoom={true}
       >
         <TileLayer
@@ -31,9 +54,25 @@ function Map() {
             </Popup>
           </Marker>
         ))}
+        <ChangeCenter position={mapPosition} />
+        <DetectClick />
       </MapContainer>
     </div>
   );
+}
+
+function ChangeCenter({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
+}
+
+function DetectClick() {
+  const navigate = useNavigate();
+  useMapEvents({
+    click: (event) =>
+      navigate(`form?lat${event.latlng.lat}&lng${event.latlng.lng}`),
+  });
 }
 
 export default Map;
